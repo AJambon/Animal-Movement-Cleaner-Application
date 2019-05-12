58631b0affd248cafe65f0efd3b55ad09e2f5d5f
Aurelie JAMBON <aurelie.jambon@agro-bordeaux.fr>
2019-05-12 17:17:38 +0200

    ajout de cesium

--- Front/stage_front/src/components/Visualization/visualization.vue
+++ Front/stage_front/src/components/Visualization/visualization.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <h1>Bienvenue dans la visualisation !!!!</h1>
+    <!-- <h1>Bienvenue dans le nouvel outil Hello world !</h1> -->
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueCesium from 'vue-cesium'
+// Vue-Cesium will load Cesium.js from `https://unpkg.com/cesium/Build/Cesium/Cesium.js`
+Vue.use(VueCesium, {
+  // cesiumPath is path of Cesium.js', for example:
+  // local Cesium Build package:
+  // cesiumPath: './static/Cesium/Cesium.js'
+  // Official Online Cesium Build package：
+  cesiumPath: 'https://unpkg.com/cesium/Build/Cesium/Cesium.js'
+})
 export default{
 }
 </script>
 
 <style>
-
 </style>
