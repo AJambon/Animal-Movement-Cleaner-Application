65d310be42cabf0dfbfcf74d5022f1cb62e206ba
Aurelie JAMBON <aurelie.jambon@agro-bordeaux.fr>
2019-08-14 18:05:29 +0200

    add type species

--- Front/stage_front/src/components/Parameters/Parameters.vue
+++ Front/stage_front/src/components/Parameters/Parameters.vue
@@ -13,6 +13,11 @@
               <option :value="{ R: '2D2' }">Another option with object value</option>
             </optgroup> -->
           </b-form-select>
+          <!-- <b-form-select v-model="parameters.technology" @change="parametersSelection" class="mb-3">
+            <option :value="null">Please select a technology</option>
+            <option value="argos">ARGOS</option>
+            <option value="gps">GPS</option>
+          </b-form-select> -->
           <b-form-input id="species" v-model="parameters.species" required placeholder="Enter species" @change="parametersSelection"></b-form-input>
           <b-form-input id="max_speed" v-model="parameters.speed" required placeholder="Enter species max speed" @change="parametersSelection"></b-form-input>
           <b-form-input id="immo_time" v-model="parameters.immoTime" required placeholder="Enter minimum immobility duration to consider it as immobility detection" @change="parametersSelection"></b-form-input>
