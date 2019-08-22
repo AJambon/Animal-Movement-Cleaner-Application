9a7147502fefbb06289d5794ba02e6e67825aa30
Aurelie JAMBON <aurelie.jambon@agro-bordeaux.fr>
2019-08-22 18:01:04 +0200

    design

--- Front/stage_front/src/components/Parameters/Parameters.vue
+++ Front/stage_front/src/components/Parameters/Parameters.vue
@@ -5,21 +5,29 @@
         <b-collapse id="collapse" visible>
           <b-form id='p'>
             <div>
-            <label>Technology</label>
-            <b-form-select v-model="parameters.technology" @change="parametersSelection">
-              <option :value="null">Please select a technology</option>
-              <option value="argos">ARGOS</option>
-              <option value="gps">GPS</option>
-            </b-form-select>
-            <b-form-select v-model="parameters.speciesType" @change="parametersSelection" class="mb-3">
-              <option :value="null">Please select a type of species</option>
-              <option value="Terrestrian">Terrestrian</option>
-              <option value="Avian">Avian</option>
-              <option value="Aquatic">Aquatic</option>
-            </b-form-select>
+              <label>Technology</label>
+              <b-form-select v-model="parameters.technology" @change="parametersSelection">
+                <option :value="null">Please select a technology</option>
+                <option value="argos">ARGOS</option>
+                <option value="gps">GPS</option>
+              </b-form-select>
+            </div>
+            <div>
+              <label>Type of species</label>
+              <b-form-select v-model="parameters.speciesType" @change="parametersSelection">
+                <option :value="null">Please select a type of species</option>
+                <option value="Terrestrian">Terrestrian</option>
+                <option value="Avian">Avian</option>
+                <option value="Aquatic">Aquatic</option>
+              </b-form-select>
+            </div>
             <div>
               <label>Species</label>
               <b-form-input id="species" v-model="parameters.species" required placeholder="Enter species" @change="parametersSelection"></b-form-input>
+            </div>
+            <div>
+              <label>Deployment date</label>
+              <b-form-input id="deployment_date" type = "datetime-local" v-model="parameters.deploymentDate" @change="parametersSelection"></b-form-input>
             </div>
             <div>
               <label>Species max speed (km/h)</label>
@@ -27,8 +35,8 @@
               <b-form-input id="max_speed" v-model="parameters.speed" required placeholder="Enter species max speed" @change="parametersSelection"></b-form-input>
               <label>Immobility min. duration (h)</label>
               <b-form-input id="immo_time" v-model="parameters.immoTime" required placeholder="Enter minimum immobility duration to consider it as immobility detection" @change="parametersSelection"></b-form-input>
-              <b-form-input id="deployment_date" type = "datetime-local" v-model="parameters.deploymentDate" @change="parametersSelection"></b-form-input>
             </div>
+
           </b-form>
         </b-collapse>
       </div>
@@ -50,11 +58,11 @@ export default {
       test: 2,
       parameters: {
         technology: 'gps',
-        speciesType: 'Terrestrian',
+        speciesType: 'Avian',
         species: 'EV',
-        speed: '52',
+        speed: '300',
         immoTime: '24',
-        deploymentDate:"2018-04-17T08:00"
+        deploymentDate:"2012-04-17T08:00"
       }
     }
   },
