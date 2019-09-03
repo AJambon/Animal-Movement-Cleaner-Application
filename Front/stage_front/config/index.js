d2041ee5d7fd4980a7283c02e9da36ad31d76636
Aurelie JAMBON <aurelie.jambon@agro-bordeaux.fr>
2019-09-03 14:39:22 +0200

    com for local prod conditions

--- Front/stage_front/config/index.js
+++ Front/stage_front/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/', //assetsPublicPath: './' for local prod conditions 
 
     /**
      * Source Maps
